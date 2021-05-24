92e90f604924cca046a4d8bfb18bd9d79438ca03
Chris Blyth <blythmeister@gmail.com>
2021-05-24 06:57:24 +0100

    Improve logging & error handling

--- src/TimelapseCreator/Program.cs
+++ src/TimelapseCreator/Program.cs
@@ -11,6 +11,7 @@ namespace TimelapseCreator
     {
         private static void Main(string[] _)
         {
+            //These could come from arguments
             var basePath = @"F:\DriveImages";
             var rawImageFolder = "Raw";
             var datedImageFolder = "Dated";
@@ -18,9 +19,21 @@ namespace TimelapseCreator
             var date = "2021-05-17";
             var timezone = "BST";
 
-            TimestampImages(basePath, rawImageFolder, datedImageFolder, date, timezone);
-            CreateVideo(basePath, datedImageFolder, videoFolder, date);
-            Console.ReadLine();
+            try
+            {
+                TimestampImages(basePath, rawImageFolder, datedImageFolder, date, timezone);
+                CreateVideo(basePath, datedImageFolder, videoFolder, date);
+                Console.WriteLine("Video Created");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Press Enter To Close...");
+                Console.ReadLine();
+            }
         }
 
         private static void TimestampImages(string basePath, string rawImageFolder, string datedImageFolder, string date, string timezone)
@@ -28,25 +41,27 @@ namespace TimelapseCreator
             var rawPath = Path.Combine(basePath, rawImageFolder, date);
             var datedPath = Path.Combine(basePath, datedImageFolder, date);
 
+            //Clean output directory
             if (Directory.Exists(datedPath))
             {
+                Console.WriteLine($"Directory {datedPath} already exists, so cleaning it");
                 Directory.Delete(datedPath, true);
             }
             Directory.CreateDirectory(datedPath);
 
             Console.WriteLine($"Outputting images from {rawPath} with timestamp");
-            foreach (var file in Directory.GetFiles(rawPath, "*.*", SearchOption.AllDirectories))
+            foreach (var file in Directory.GetFiles(rawPath, "*.jpg", SearchOption.TopDirectoryOnly))
             {
                 //Load the Image to be written on.
                 using (var bitMapImage = new Bitmap(file))
+                using (var graphicImage = Graphics.FromImage(bitMapImage))
                 {
-                    var graphicImage = Graphics.FromImage(bitMapImage);
-
                     var timestampText = file.Replace(rawPath, "").Substring(1).Replace(".jpg", "");
-                    var timestamp = DateTime.ParseExact(timestampText, "yyMMdd_HHmm", CultureInfo.InvariantCulture);
+                    var timestamp = DateTime.ParseExact(timestampText, "yyMMdd_HHmm", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
 
                     //Draw rectangle
                     graphicImage.FillRectangle(new SolidBrush(Color.Black), 895, 675, 500, 100);
+                    //Write timestamp
                     graphicImage.DrawString($"{timestamp:yyyy-MM-dd HH:mm} {timezone}", new Font("Consolas", 24, FontStyle.Bold), new SolidBrush(Color.White), 900, 680);
 
                     //Save the new image
@@ -63,31 +78,24 @@ namespace TimelapseCreator
             var videoFolderPath = Path.Combine(basePath, videoFolder);
             var videoPath = Path.Combine(videoFolderPath, $"{date}.avi");
 
+            Directory.CreateDirectory(videoFolderPath);
+
             if (File.Exists(videoPath))
             {
+                Console.WriteLine($"Video file {videoPath} already exists, so removing it");
                 File.Delete(videoPath);
             }
 
-            Directory.CreateDirectory(videoFolderPath);
-
-            var files = Directory.GetFiles(datedPath);
-
-            int frameHeight;
-            int frameWidth;
-            var frameRate = 30;
-
-            using (var firstImage = Image.FromFile(files[0]))
-            {
-                frameHeight = firstImage.Height;
-                frameWidth = firstImage.Width;
-            }
-
-            Console.WriteLine($"Creating video from images in {datedPath}.  Size: {frameHeight}x{frameWidth} @ {frameRate}fps");
-
             using (var writer = new VideoFileWriter())
             {
-                writer.Open(videoPath, frameWidth, frameHeight, frameRate, VideoCodec.Raw);
+                //Hardcoded size and framerate assuming input images came from bash script
+                writer.Open(videoPath, 1280, 720, 30, VideoCodec.Raw);
 
+                var files = Directory.GetFiles(datedPath);
+
+                Console.WriteLine($"Creating video from {files.Length} images in {datedPath}.");
+
+                //Add all images as a frame in the video
                 foreach (var file in files)
                 {
                     using (var bitmap = (Bitmap)Image.FromFile(file))
@@ -99,7 +107,7 @@ namespace TimelapseCreator
                 writer.Close();
             }
 
-            Console.WriteLine($"Video created {videoPath}");
+            Console.WriteLine($"Video saved to {videoPath}");
         }
     }
 }
