3d64f960d4665ce29960fdef629032815b3c04a4
Chris Blyth <blythmeister@gmail.com>
2021-05-26 09:47:36 +0100

    Update script to use 5 frames to make the image.
    10 frames leads to some weird bluring when people actually moving

--- src/ImageTaker/script.sh
+++ src/ImageTaker/script.sh
@@ -6,7 +6,7 @@ do
   TIME=$(date +"%y%m%d_%H%M")
   echo "Taking photo to file $DATE/$TIME"
   mkdir /home/chris/drive/photos/$DATE
-  fswebcam --device /dev/video0 --resolution 1280x720 --no-banner --delay 2 --skip 40 --frames 10 /home/chris/drive/photos/$DATE/$TIME.jpg
+  fswebcam --device /dev/video0 --resolution 1280x720 --no-banner --delay 2 --skip 40 --frames 5 /home/chris/drive/photos/$DATE/$TIME.jpg
   echo "Sleeping 1 minutes before next photo..."
   sleep 1m
 done
