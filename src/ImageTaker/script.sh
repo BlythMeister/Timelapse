4e805f692cb235c091f3b080440608d8c5cfc6cd
Chris Blyth <blythmeister@gmail.com>
2021-06-04 21:50:30 +0100

    Copy photo as "latest"

--- src/ImageTaker/script.sh
+++ src/ImageTaker/script.sh
@@ -7,6 +7,8 @@ do
   echo "Taking photo to file $DATE/$TIME"
   mkdir /home/chris/drive/photos/$DATE
   fswebcam --device /dev/video0 --resolution 1280x720 --no-banner --delay 2 --skip 40 --frames 5 /home/chris/drive/photos/$DATE/$TIME.jpg
+  echo "Copy photo to latest"
+  cp /home/chris/drive/photos/$DATE/$TIME.jpg /home/chris/drive/latest.jpg
   echo "Sleeping 1 minutes before next photo..."
   sleep 1m
 done
